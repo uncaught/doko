d73ac479bec0aec22414b6cb69a9fdf1a62a2f9c
uncaught <uncaught42@gmail.com>
2020-01-07 08:45:25 +0100

    Switch to rows to keep them aligned

--- client/src/pages/game/Announcements.tsx
+++ client/src/pages/game/Announcements.tsx
@@ -2,7 +2,7 @@ import React, {ReactElement} from 'react';
 import Announcing from './Announcing';
 
 export default function Announcements({isRe}: { isRe: boolean }): ReactElement {
-  return <div className="u-flex-row u-flex-wrap">
+  return <div className="u-flex-row u-flex-wrap u-margin-top-tiny">
     <Announcing type={'announced'} label={isRe ? 'Re' : 'Contra'} isRe={isRe}/>
     <div className="u-flex-row u-flex-wrap">
       <Announcing type={'no9'} label={'9'} text={'keine 9'} isRe={isRe}/>
--- client/src/pages/game/NonPenalty.tsx
+++ client/src/pages/game/NonPenalty.tsx
@@ -1,15 +1,33 @@
 import React, {ReactElement} from 'react';
 import {Divider, Grid, Segment} from 'semantic-ui-react';
-import Side from './Side';
 import UndecidedPlayers from './UndecidedPlayers';
 import GameCalcBlame from './GameCalcBlame';
+import Announcements from './Announcements';
+import ExtraPoints from './ExtraPoints';
+import SidePlayers from './SidePlayers';
+import GamePips from './GamePips';
+import Points from './Points';
+
+function Row({Comp}: { Comp: React.FC<{ isRe: boolean }> }): ReactElement {
+  return <Grid.Row>
+    <Grid.Column>
+      <Comp isRe={true}/>
+    </Grid.Column>
+    <Grid.Column>
+      <Comp isRe={false}/>
+    </Grid.Column>
+  </Grid.Row>;
+}
 
 export default function NonPenalty(): ReactElement {
   return <>
     <Segment vertical>
       <Grid columns={2} relaxed='very' className="tinyVertical">
-        <Side isRe={true}/>
-        <Side isRe={false}/>
+        <Row Comp={Announcements}/>
+        <Row Comp={ExtraPoints}/>
+        <Row Comp={SidePlayers}/>
+        <Row Comp={GamePips}/>
+        <Row Comp={Points}/>
       </Grid>
       <Divider vertical>VS</Divider>
     </Segment>
