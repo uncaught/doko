ffbee2aef4f19e9fda2f78e19b152b4ec1374ff0
uncaught <uncaught42@gmail.com>
2020-01-11 16:13:57 +0100

    Switch to css grid for table to utilize sticky header.

--- client/src/pages/games/Games.tsx
+++ client/src/pages/games/Games.tsx
@@ -1,5 +1,5 @@
 import React, {ReactElement} from 'react';
-import {Icon, Table} from 'semantic-ui-react';
+import {Icon} from 'semantic-ui-react';
 import {useRoundParticipatingPlayers} from '../../store/Players';
 import {useSortedGames} from '../../store/Games';
 import {useGroupMembers} from '../../store/GroupMembers';
@@ -7,6 +7,7 @@ import {useHistory, useRouteMatch} from 'react-router-dom';
 import AddGame from '../round/AddGame';
 import {GameData, soloGameTypes} from '@doko/common';
 import RoundEndInfo from '../round/RoundEndInfo';
+import classnames from 'classnames';
 
 function RoundIndicators({data}: { data: GameData }): ReactElement {
   const indicators: ReactElement[] = [];
@@ -32,59 +33,70 @@ export default function Games(): ReactElement {
   const history = useHistory();
   const {url} = useRouteMatch();
   let lastRunNumber = 0;
+  const rowCells: ReactElement[] = [];
+
+  games.forEach(({id, gameNumber, data, dealerGroupMemberId}) => {
+    const {gamePoints, re, contra, isComplete, winner, runNumber} = data;
+    const isNewRun = lastRunNumber !== runNumber;
+    lastRunNumber = runNumber;
+
+    const rowClasses = ['grid-table-td'];
+    const rowOnClick = () => history.push(`${url}/game/${id}`);
+
+    if (isNewRun) {
+      rowClasses.push('newRunRow');
+    }
+    const rowCss = rowClasses.join(' ');
+
+    rowCells.push(<div key={`num_${gameNumber}`} className={rowCss} onClick={rowOnClick}>
+      {gameNumber}
+      <RoundIndicators data={data}/>
+    </div>);
+
+    rowCells.push(<div key={`dealer_${gameNumber}`} className={rowCss} onClick={rowOnClick}>
+      {members[dealerGroupMemberId].name[0]}
+    </div>);
+
+    rowCells.push(<div key={`points_${gameNumber}`} className={rowCss} onClick={rowOnClick}>
+      {isComplete ? gamePoints : ''}
+    </div>);
+
+    players.forEach((p) => {
+      const isRe = re.members.includes(p.groupMemberId);
+      const isContra = !isRe && contra.members.includes(p.groupMemberId);
+
+      if ((!isRe && !isContra) || !isComplete) {
+        rowCells.push(<div key={`cell_${gameNumber}_${p.groupMemberId}`} className={rowCss} onClick={rowOnClick}>
+          {isComplete ? '-' : ''}
+        </div>);
+        return;
+      }
+
+      const points = data[isRe ? 're' : 'contra'].totalPoints;
+      const newPoints = (sumMap.get(p.groupMemberId) || 0) + points;
+      sumMap.set(p.groupMemberId, newPoints);
+
+      const hasWon = (winner === 're' && isRe) || (winner === 'contra' && isContra);
+      const hasLost = !hasWon && winner !== 'stalemate';
+
+      rowCells.push(<div key={`cell_${gameNumber}_${p.groupMemberId}`}
+                         className={classnames(rowCss, {'won': hasWon, 'lost': hasLost})}
+                         onClick={rowOnClick}>
+        {newPoints}
+        <RoundPlayerIndicators data={data} memberId={p.groupMemberId}/>
+      </div>);
+    });
+  });
 
   return <section>
-    <Table basic unstackable className="gamesTable" textAlign='center'>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell collapsing>#</Table.HeaderCell>
-          <Table.HeaderCell collapsing><Icon name={'hand paper outline'}/></Table.HeaderCell>
-          <Table.HeaderCell collapsing><Icon name={'bullseye'}/></Table.HeaderCell>
-          {players.map((p) =>
-            <Table.HeaderCell key={p.groupMemberId}>{members[p.groupMemberId].name[0]}</Table.HeaderCell>)}
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {games.map(({id, gameNumber, data, dealerGroupMemberId}) => {
-          const {gamePoints, re, contra, isComplete, winner, runNumber} = data;
-          const isNewRun = lastRunNumber !== runNumber;
-          lastRunNumber = runNumber;
-          return <Table.Row key={id}
-                            className={isNewRun ? 'newRunRow' : ''}
-                            onClick={() => history.push(`${url}/game/${id}`)}>
-            <Table.Cell className="u-relative">
-              {gameNumber}
-              <RoundIndicators data={data}/>
-            </Table.Cell>
-            <Table.Cell>{members[dealerGroupMemberId].name[0]}</Table.Cell>
-            <Table.Cell>{isComplete ? gamePoints : ''}</Table.Cell>
-            {players.map((p) => {
-              const isRe = re.members.includes(p.groupMemberId);
-              const isContra = !isRe && contra.members.includes(p.groupMemberId);
-
-              if ((!isRe && !isContra) || !isComplete) {
-                return <Table.Cell key={p.groupMemberId}>{isComplete ? '-' : ''}</Table.Cell>;
-              }
-
-              const points = data[isRe ? 're' : 'contra'].totalPoints;
-              const newPoints = (sumMap.get(p.groupMemberId) || 0) + points;
-              sumMap.set(p.groupMemberId, newPoints);
-
-              const hasWon = (winner === 're' && isRe) || (winner === 'contra' && isContra);
-              const hasLost = !hasWon && winner !== 'stalemate';
-
-              return <Table.Cell positive={hasWon}
-                                 negative={hasLost}
-                                 key={p.groupMemberId}
-                                 className="u-relative">
-                {newPoints}
-                <RoundPlayerIndicators data={data} memberId={p.groupMemberId}/>
-              </Table.Cell>;
-            })}
-          </Table.Row>;
-        })}
-      </Table.Body>
-    </Table>
+    <div className="grid-table gamesTable u-text-center" style={{gridTemplateColumns: `repeat(3, 2.5em) repeat(${players.length}, auto)`}}>
+      <div className="grid-table-th">#</div>
+      <div className="grid-table-th"><Icon name={'hand paper outline'}/></div>
+      <div className="grid-table-th"><Icon name={'bullseye'}/></div>
+      {players.map((p) =>
+        <div className="grid-table-th" key={p.groupMemberId}>{members[p.groupMemberId].name[0]}</div>)}
+      {rowCells}
+    </div>
 
     <RoundEndInfo/>
     <AddGame/>
