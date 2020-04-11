d731a499117f3b82ff57ab9bd5d8928312670624
uncaught <uncaught42@gmail.com>
2020-04-11 15:34:42 +0200

    Activate service worker and add a refresh menu item.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -47,4 +47,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://create-react-app.dev/docs/making-a-progressive-web-app/
-serviceWorker.unregister();
+serviceWorker.register();
--- client/src/pages/groups/index.tsx
+++ client/src/pages/groups/index.tsx
@@ -1,18 +1,25 @@
 import React, {ReactElement} from 'react';
 import AddGroup from './AddGroup';
-import {Divider} from 'semantic-ui-react';
+import {Divider, Icon, Menu} from 'semantic-ui-react';
 import MyGroups from './MyGroups';
 import {Switch} from 'react-router-dom';
 import Page from '../../Page';
 import Group from '../group';
 import CashInvitation from './CashInvitation';
 
+function ForceReloadMenuItem(): ReactElement {
+  return <Menu.Item onClick={async () => window.location.reload()}>
+    <Icon name='refresh'/>
+    Seite neu laden
+  </Menu.Item>;
+}
+
 export default function GroupsIndex(): ReactElement {
   return <Switch>
     <Page path={`/group/:groupId`} displayName={'Gruppe'}>
       <Group/>
     </Page>
-    <Page path={'/'}>
+    <Page path={'/'} menuItems={[ForceReloadMenuItem]}>
       <MyGroups/>
       <Divider section/>
       <AddGroup/>
