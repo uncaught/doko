6ecdd6d716da98f258b58fa3f4ac51dc383e8f77
uncaught <uncaught42@gmail.com>
2020-04-25 12:09:26 +0200

    Add popup showing remaining bock games. (closes #13)

--- client/src/pages/game/GameLabelBock.tsx
+++ client/src/pages/game/GameLabelBock.tsx
@@ -1,15 +1,50 @@
 import React, {ReactElement} from 'react';
-import {Icon, Label} from 'semantic-ui-react';
-import {useGame} from '../../store/Games';
+import {Icon, Label, Popup} from 'semantic-ui-react';
+import {useGame, useSortedGames} from '../../store/Games';
+import {useRound} from '../../store/Rounds';
+import {useRoundParticipatingPlayers} from '../../store/Players';
+
+function Remaining(): ReactElement {
+  const sortedGames = useSortedGames();
+  const game = useGame()!;
+  const roundParticipatingPlayers = useRoundParticipatingPlayers();
+  const playerCount = roundParticipatingPlayers.length;
+  const gameIndex = game.gameNumber - 1;
+  let bockStartIndex = -1;
+  let soloCount = game.data.gameType === 'dutySolo' ? 1 : 0;
+  for (let i = gameIndex - 1; i >= 0; i--) {
+    const curGame = sortedGames[i];
+    if (curGame) {
+      if (curGame.data.gameType === 'dutySolo') {
+        soloCount++;
+      }
+      if (curGame.data.qualifiesNewBockGames) {
+        bockStartIndex = i + 1;
+        break;
+      }
+    }
+  }
+  if (bockStartIndex > -1) {
+    const number = gameIndex - bockStartIndex + 1;
+    return <div>
+      <div>Bockspiel #{number}</div>
+      <div>Voraussichtlich noch {playerCount - number + soloCount}</div>
+    </div>;
+  }
+  return <div>???</div>;
+}
 
 export default function GameLabelBock(): ReactElement | null {
+  const popupDisabled = useRound()!.data.bockInBockBehavior !== 'restart';
   const {data} = useGame()!;
   if (!data.bockGameWeight) {
     return null;
   }
-  return <div className="memberDetail">
-    <Label className={data.bockGameWeight > 1 ? '' : 'iconOnly'} color={'purple'}>
-      {data.bockGameWeight > 1 ? data.bockGameWeight : ''} <Icon name={'btc'}/>
-    </Label>
-  </div>;
+  return <Popup content={<Remaining/>} pinned on={'click'} position={'bottom center'} disabled={popupDisabled} trigger={
+    <div className="memberDetail">
+      <Label className={data.bockGameWeight > 1 ? '' : 'iconOnly'} color={'purple'}>
+        {data.bockGameWeight > 1 ? data.bockGameWeight : ''} <Icon name={'btc'}/>
+      </Label>
+    </div>
+  }/>;
 }
